6c45cb1d08c65036c89a3450bd323c254e960481
Gabriel Borrás Serra <bielet40@gmail.com>
2025-10-04 00:55:10 +0200

    🔒 Stop tracking environment files (now in .gitignore)

--- src/app/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  allowRegistration: false,
-  supabaseUrl: 'https://krgffsbmwzrsdgdjdqap.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyZ2Zmc2Jtd3pyc2RnZGpkcWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyMTA3NjMsImV4cCI6MjA1Nzc4Njc2M30.FDqSOP6umhZv_AKpmZscL6x2EAkUy2V1Ga0Mwar5VlM',
-  rawgApiKey: '4afc7908a8e64f72afdfc0c10d666dcc'
-};
